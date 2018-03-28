bd938a125dbe1921fa767f0fc3de70691ecad9b9
Svetozar Nesic <snesic@gmail.com>
2018-03-28 13:43:25 +0200

    Add phasing vcf

--- runMBASED.R
+++ runMBASED.R
@@ -10,22 +10,26 @@ args <- commandArgs(trailingOnly = TRUE)
 
 noSim=1000
 if(length(args)>2){
-  noSim=as.integer(args[3])
+    noSim=as.integer(args[3])
 }
 
 
 fileInput=args[1]
 fileOutput=args[2]
-
 vcf = readVcf(fileInput)
+vcf2 = readVcf(fileInputPhased)
 
 # Filter SNP in exons
 
 vcfSNP <- vcf[isSNV(vcf)]
+vcfP <- vcfP[vcfSNP]
+vcfP <- vcfP[geno(vcfP)$Gt!='1|1']
+vcfSNP<-vcfSNP[vcfP]
+
 
 exonGeneName <- function(x){
-  s = x[grepl('exon',x)]
-  strsplit(s[1], '|', fixed=TRUE)[[1]][5]
+    s = x[grepl('exon',x)]
+    strsplit(s[1], '|', fixed=TRUE)[[1]][5]
 }
 
 
@@ -40,39 +44,40 @@ d = data.frame(rowData(v)['REF'], rowData(v)['ALT'], geno(v)$GT)
 colnames(d) <- c('ph1', 'ph2', 'GT')
 
 ## sort columns ph1 and ph2 on GT=='0|1' or '1|0'
-## pass d$ph1 as allele1 and d$ph2 as allele2 
+## pass d$ph1 as allele1 and d$ph2 as allele2
 
 ##-----------MBASED--Prepare samples-------------------
 
 mySNVs <- GRanges(seqnames=as.character(seqnames(vcfSNP)),
-                  ranges=ranges(rowRanges(vcfSNP)),
-                  aseID=geneList,
-                  allele1=as.character(unlist(rowData(vcfSNP)[,'ALT'])),
-                  allele2=as.character(rowData(vcfSNP)[,'REF']))
+ranges=ranges(rowRanges(vcfSNP)),
+aseID=geneList,
+allele1=as.character(unlist(rowData(vcfSNP)[,'ALT'])),
+allele2=as.character(rowData(vcfSNP)[,'REF']))
 
 
 
 mySample <- SummarizedExperiment(assays=list(
-  lociAllele1Counts=matrix(as.integer(info(vcfSNP)$AC), ncol=1, dimnames=list(names(mySNVs), 'mySample')),
-  lociAllele2Counts=matrix(as.integer(info(vcfSNP)$AN-info(vcfSNP)$AC), ncol=1, dimnames=list(names(mySNVs), 'mySample'))), 
-  rowRanges=mySNVs)
+lociAllele1Counts=matrix(as.integer(info(vcfSNP)$AC), ncol=1, dimnames=list(names(mySNVs), 'mySample')),
+lociAllele2Counts=matrix(as.integer(info(vcfSNP)$AN-info(vcfSNP)$AC), ncol=1, dimnames=list(names(mySNVs), 'mySample'))),
+rowRanges=mySNVs)
 
 #----Run MBASED
 
 ASEresults_1s <- runMBASED(
-  ASESummarizedExperiment=mySample,
-  isPhased=FALSE,
-  numSim=noSim,
-  BPPARAM = MulticoreParam())#SerialParam())
+ASESummarizedExperiment=mySample,
+isPhased=FALSE,
+numSim=noSim,
+BPPARAM = MulticoreParam())#SerialParam())
 
 #------Write results------
 
-df = data.frame(assays(ASEresults_1s)$majorAlleleFrequency, 
-                assays(ASEresults_1s)$pValueASE, 
-                assays(ASEresults_1s)$pValueHeterogeneity)
+df = data.frame(assays(ASEresults_1s)$majorAlleleFrequency,
+assays(ASEresults_1s)$pValueASE,
+assays(ASEresults_1s)$pValueHeterogeneity)
 
 
 df <- cbind(newColName = rownames(df), df)
 
 colnames(df)=c('Gene', 'majorAlleleFrequency','pValueASE','pValueHeterogeneity')
 write.table(df,fileOutput, quote=FALSE, row.names = FALSE, sep=" ")
+
