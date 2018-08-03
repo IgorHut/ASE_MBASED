055c4f49605ab3785f5eb37091cc03e617deccb5
Svetozar Nesic <snesic@gmail.com>
2018-08-03 12:59:02 +0200

    Add info update

--- add_ph_info_to_vcf.R
+++ add_ph_info_to_vcf.R
@@ -1,25 +1,26 @@
+suppressPackageStartupMessages({
+  library(VariantAnnotation)
+  library(SummarizedExperiment)
+  library(GenomicRanges) })
+args <- commandArgs(trailingOnly = TRUE)
 
+fileIn=args[1]
+fileInPh=args[2]
+fileOutPh = substr(fileInPh,1, nchar(fileInPh)-3)
+
+# Load in two neede .vcf files vep.vcf and vep.phased.vcf
+vcf_vep = readVcf(fileIn)
+vcf_phased = readVcf(fileInPh)
 
 geno(vcf_vep)@listData$PH_info = geno(vcf_phased)$GT
-vcf_vep
-geno(vcf_vep)
-geno(vcf_vep)$PH_info
 
-writeVcf(vcf_vep, "vcf_with_ph_info.vcf")
+# Make the new info fields.
+newInfo <- DataFrame(Number=1, Type="String",
+                     Description="Phasing information",
+                     row.names="PH_info")
 
+# Add the new info fields
+geno(header(vcf_vep)) <- rbind(geno(header(vcf_vep)), newInfo)
 
-header(vcf_vep)@header@listData$FORMAT@rownames <- 
-  c(header(vcf_vep)@header@listData$FORMAT@rownames, "PH_info")
-
-header(vcf_vep)@header@listData$FORMAT@nrows <- integer(6)
-
-header(vcf_vep)@header@listData$FORMAT@listData$Number <- 
-  c(header(vcf_vep)@header@listData$FORMAT@listData$Number, 1)
-
-header(vcf_vep)@header@listData$FORMAT@listData$Type <- 
-  c(header(vcf_vep)@header@listData$FORMAT@listData$Type, "String")
-
-header(vcf_vep)@header@listData$FORMAT@listData$Description <- 
-  c(header(vcf_vep)@header@listData$FORMAT@listData$Description, "Phasing information")
-
-geno(header(vcf_vep))
+#Write VCF 
+writeVcf(vcf_vep, fileOutPh)
